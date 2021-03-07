6a5be20f81c2970b77ca6855b2cd8168891c1f25
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-07 19:59:41 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,18 +1,18 @@
 board=[" "," "," "," "," "," "," "," "," ",]
 
+def valid_move? (board, index)
+  position_taken?
+  if !position_taken? && index>=0 && index<=8
+    true
+  else
+    false
+  end
+end
+
 def position_taken? (board, index)
   if board[index]==" " || board[index]=="" || board[index]==nil
     false
   else
     true
-  end
-end
-
-def valid_move? (board, index)
-  position_taken?
-  if !!position_taken?==false && index>=0 && index<=8
-    true
-  else
-    false
   end
 end
